348c20f41538ecb054141b35631b59850bfb661c
Jaewon Lee <jlee38266@gmail.com>
2025-01-15 17:07:19 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
@@ -0,0 +1,16 @@
+def solution(num_list):
+    answer = 0
+    case1 = 0
+    case2 = 1
+    
+    for num in num_list:
+        case1 += num
+        case2 *= num
+    # print(case1**2)
+    # print(case2)
+    if case1**2 > case2:
+        return 1
+    else:
+        answer
+    
+    return answer
