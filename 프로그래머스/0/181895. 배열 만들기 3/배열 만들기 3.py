d38f46b2aa6b338235fdc439eb6bbed1641fdf47
Jaewon Lee <jlee38266@gmail.com>
2025-01-23 20:49:00 +0900

    Update 배열 만들기 3.py

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.py
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.py
@@ -1,6 +1,9 @@
 def solution(arr, intervals):
     answer = []
-    
+
+
+    # extend: 기존 리스트 객체에 요소 추가 (메모리 효율적)
+    # += : 새로운 리스트 객체 생성 후 할당 (추가 메모리 사용)
     for start, end in intervals:
         answer.extend(arr[start:end+1])
     
