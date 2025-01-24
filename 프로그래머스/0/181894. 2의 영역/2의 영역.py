e60d104f577ec61b519cb0fc1c23de54c4f1e993
Jaewon Lee <jlee38266@gmail.com>
2025-01-24 14:39:32 +0900

    Update 2의 영역.py

--- 프로그래머스/0/181894. 2의 영역/2의 영역.py
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.py
@@ -1,13 +1,23 @@
+from collections import deque
+
 def solution(arr):
-    left, right = 0, len(arr) - 1
+#     left, right = 0, len(arr) - 1
     
-    while left <= right and arr[left] != 2:
-        left += 1
+#     while left <= right and arr[left] != 2:
+#         left += 1
         
-    while left <= right and arr[right] != 2:
-        right -= 1
+#     while left <= right and arr[right] != 2:
+#         right -= 1
         
-    if left > right:
-        return [-1]
+#     if left > right:
+#         return [-1]
         
-    return arr[left:right + 1]
+#     return arr[left:right + 1]
+
+    dq = deque(arr)
+    while dq and dq[0] != 2:
+        dq.popleft()
+    while dq and dq[-1] != 2:
+        dq.pop()
+
+    return list(dq) if dq else [-1]
