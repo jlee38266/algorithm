5c2c7893112698500bc9a21cfb9b97572d44da6c
Jaewon Lee <jlee38266@gmail.com>
2025-02-01 22:06:15 +0900

    Update 배열의 원소만큼 추가하기.py

--- 프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.py
+++ 프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.py
@@ -1,7 +1,11 @@
 def solution(arr):
     answer = []
     
-    for num in arr:
-        answer += [num] * num
+    # for num in arr:
+    #     answer += [num] * num
+        
+    for num in arr:  
+        for _ in range(num):
+            answer.append(num)
             
     return answer
