df89b830fac460d0e6e0fd35a0ae6fe6eb09d5a6
Jaewon Lee <jlee38266@gmail.com>
2025-02-06 06:39:44 +0900

    [level 0] Title: l로 만들기, Time: 8.40 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.py
@@ -0,0 +1,10 @@
+def solution(myString):
+    answer = ''
+    
+    for char in myString:
+        if char < 'l':
+            answer += 'l'
+        else:
+            answer += char
+    
+    return answer
